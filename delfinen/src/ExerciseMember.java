65a01a99228e01be510cf52b5e4b0d4f08fc0508
Fladbrod <73465595+Fladbrod@users.noreply.github.com>
2020-11-24 12:20:50 +0100

    Addet på exercisemember klassen

--- delfinen/src/ExerciseMember.java
+++ delfinen/src/ExerciseMember.java
@@ -1,2 +1,21 @@
-public class ExerciseMember extends Member{
-}
+public class ExerciseMember extends Member {
+
+    public ExerciseMember(int age, String name) {
+        this.age = age;
+        this.name = name;
+        if (age > 60) {
+            membershipFee = SENIOR_PRICE * SENIOR_DISCOUNT;
+        } else if (age > 18) {
+            membershipFee = SENIOR_PRICE;
+        } else {
+            membershipFee = JUNIOR_PRICE;
+        }
+    }
+
+
+        @Override
+        void setMembershipFee() {
+
+        }
+    }
+
--- delfinen/src/PassiveMember.java
+++ delfinen/src/PassiveMember.java
@@ -1,2 +1,13 @@
 public class PassiveMember extends Member {
+
+    public PassiveMember() {
+        super.membershipFee = 500;
+    }
+
+    @Override
+    void setMembershipFee() {
+
+    }
 }
+
+
