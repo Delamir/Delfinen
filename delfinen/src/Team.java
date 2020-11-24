95b51d71823272dfea4bec79e8c765f55af4865e
Kringelum2 <p.kringelum@gmail.com>
2020-11-24 14:01:45 +0100

    menu
    menu i team

--- delfinen/src/Team.java
+++ delfinen/src/Team.java
@@ -1,9 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Team {
 
+    private final String MENU_HEADER = "Register tournament";
+    private final String LEAD_TEXT = "Please choose: ";
+    private final String[] MENU_POINT = {"1. Register tournament", "2. Show swim results",
+            "3. Appoint tournament participants", "4. Register swimming results"};
+    ArrayList<Member> memberList = new ArrayList<>();
+
+    IO io = new IO("MemberList");
+    Menu menu = new Menu(MENU_HEADER, LEAD_TEXT, MENU_POINT);
+
+    Scanner in = new Scanner(System.in);
+
+    public void showMenu() {
+        boolean logOut = false;
+        Results results = new Results();
+
+        while (!logOut) {
+            menu.printMenu();
+            switch (menu.readChoice()) {
+                case 1:
+                    registerTournament();
+                    break;
+                case 2:
+                    results.showResults();
+                    break;
+                case 3:
+                    appointParticipant();
+                    break;
+                case 4:
+                    registerResult();
+                    break;
+                case 9:
+                    logOut = true;
+                default:
+                    System.out.print("Not a valid menu point, please try again: ");
+
+            }
+        }
+    }
 
     public void registerTournament() {
 
+    }
+
+    public void appointParticipant() {
 
     }
 
+    public void registerResult(){
+
+    }
 }
