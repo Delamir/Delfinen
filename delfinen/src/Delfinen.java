dbe2fbf5a43ad72f878355fa1b5a869531b35d8a
Delamir <71318139+Delamir@users.noreply.github.com>
2020-11-24 12:42:58 +0100

    MOARE Delfinen stuff

--- delfinen/src/Delfinen.java
+++ delfinen/src/Delfinen.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Delfinen {
 
@@ -9,6 +10,8 @@ public class Delfinen {
 
     IO io = new IO();
     Menu menu = new Menu(MENU_HEADER, LEAD_TEXT, MENU_POINT);
+
+    Scanner in = new Scanner(System.in);
 
     public void showMenu() {
         boolean logOut = false;
@@ -36,14 +39,43 @@ public class Delfinen {
     }
 
     public void registerMember() {
+        String name;
+        int age;
 
+        System.out.println("Register new member");
+        System.out.print("Enter the name: ");
+        name = in.nextLine();
+        System.out.print("Enter age: ");
+        age = (int) validNumberInput(0, 100, "Not a valid age, please input a valid age: ");
     }
 
     public void totalRevenue() {
 
     }
 
-    public void run() {
-        showMenu();
+    public double validNumberInput(double numberFrom, double numberTo, String errorMessage) {
+        boolean validChoice = false;
+        double legalNumber = -1;
+
+        while (!validChoice) {
+            if (in.hasNextDouble()) {
+                legalNumber = in.nextDouble();
+                in.nextLine(); //Scanner bug
+                if (legalNumber >= numberFrom && legalNumber <= numberTo) {
+                    validChoice = true;
+                } else {
+                    System.out.println(errorMessage);
+                }
+            } else {
+                System.out.println(errorMessage);
+                in.nextLine();
+            }
+        }
+        return legalNumber;
     }
-}
+
+
+        public void run () {
+            showMenu();
+        }
+    }
