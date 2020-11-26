9ed552b6f044dbb15d2167272e148f5bb21f5a42
Sverri <sverrireinert@gmail.com>
2020-11-26 12:35:37 +0100

    ExerciseMemberTest

--- delfinen/src/ExerciseMember.java
+++ delfinen/src/ExerciseMember.java
@@ -3,12 +3,12 @@ public class ExerciseMember extends Member {
      * Method that determines the price for a member based on age.
      * @param age determines if the member is senior, junior or hella old
      * @param name checks the name of the member
-     * @Author Sverri & Joachim
+     * @author Sverri & Joachim
      */
     public ExerciseMember(int age, String name) {
         super(age, name);
         if (age > 60) {
-            setMembershipFee(SENIOR_PRICE * SENIOR_DISCOUNT);
+            setMembershipFee(SENIOR_PRICE - (SENIOR_PRICE * SENIOR_DISCOUNT));
         } else if (age > 18) {
             setMembershipFee(SENIOR_PRICE);
         } else {
