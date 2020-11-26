a27c17b0a6965d6155c11bad519d460c67ab2cf4
Sverri <sverrireinert@gmail.com>
2020-11-26 12:36:46 +0100

    ExerciseMemberTest

--- delfinen/test/ExerciseMemberTest.java
+++ delfinen/test/ExerciseMemberTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ExerciseMemberTest {
 
     @Test
-    void ExceciseMemberTest(){
+    void exceciseMemberTest(){
         Member member1 = new ExerciseMember(10,"Hans");
         Member member2 = new ExerciseMember(20,"Jens");
         Member member3 = new ExerciseMember(66,"Poul");
