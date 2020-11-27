27d4edc4f8c18798c74cfc6888c18bf397a1e370
Sverri <sverrireinert@gmail.com>
2020-11-27 10:50:25 +0100

    team test refactor

--- delfinen/test/TeamTest.java
+++ delfinen/test/TeamTest.java
@@ -5,7 +5,7 @@ class TeamTest {
 
     @Test
     void registerResult() {
-        Team t = new Team();
+        Team t = new Team(null, null);
         ByteArrayInputStream in = new ByteArrayInputStream("12\n23\n25\n666\n\n23\n25\n12\n\n23\nki\n".getBytes());
         System.setIn(in);
         t.registerResult();
