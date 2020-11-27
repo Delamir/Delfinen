a087cd395fda41002164ff5b1341d0da0c32c283
Delamir <71318139+Delamir@users.noreply.github.com>
2020-11-27 10:27:26 +0100

    MORE CASHIER

--- delfinen/src/Cashier.java
+++ delfinen/src/Cashier.java
@@ -1,3 +1,42 @@
 public class Cashier {
 
+    private static final String MENU_HEADER = "Welcome Chairman";
+    private static final String LEAD_TEXT = "Please choose: ";
+    private static final String[] MENU_POINT = {"1. Subscription Overview", "2. Arrears", "9. Log out"};
+
+    Menu menu = new Menu(MENU_HEADER, LEAD_TEXT, MENU_POINT);
+
+    public void showMenu() {
+        boolean logOut = false;
+
+        while(!logOut) {
+            menu.printMenu();
+            switch(menu.readChoice()) {
+                case 1:
+                    subscriptionOverview();
+                    break;
+                case 2:
+                    arrears();
+                    break;
+                case 9:
+                    System.out.println("Logging out. Have a nice day");
+                    logOut = true;
+                    break;
+                default:
+                    System.out.print("Not a valid menu point, please try again: ");
+            }
+        }
+    }
+
+    public void arrears() {
+
+    }
+
+    public void subscriptionOverview() {
+
+    }
+
+    public void run() {
+        showMenu();
+    }
 }
--- delfinen/src/RunProgramMenu.java
+++ delfinen/src/RunProgramMenu.java
@@ -39,7 +39,7 @@ public class RunProgramMenu {
 
         final String MENU_HEADER = "Delfinen Swimming Club";
         final String LEAD_TEXT = "Please choose: ";
-        final String[] MENU_POINT = {"1. Register Member - Chairman", "2. For Cashier Menu", "3. Coach", "9. Log out"};
+        final String[] MENU_POINT = {"1. Register Member - Chairman", "2. Coach", "3. For Cashier", "9. Log out"};
         boolean logOut = false;
 
         Menu menu = new Menu(MENU_HEADER, LEAD_TEXT, MENU_POINT);
@@ -54,6 +54,7 @@ public class RunProgramMenu {
                     teamMenu();
                     break;
                 case 3:
+                    new Cashier().run();
                     break;
                 case 9:
                     System.out.println("Logging out. Have a nice day");
