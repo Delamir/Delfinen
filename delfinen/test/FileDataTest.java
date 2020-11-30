fd6b59b33c9d47a91f6281d8893f6c4eeb6490d7
Kringelum2 <p.kringelum@gmail.com>
2020-11-30 11:59:14 +0100

    Noget FileDataTest lort fuck junit

--- delfinen/test/FileDataTest.java
+++ delfinen/test/FileDataTest.java
@@ -1,8 +1,25 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileDataTest {
+
+    @Test
+    void FileDataTest() {
+        ArrayList<String> testData = new ArrayList<>();
+        String fileNameTest = "test.ser";
+        testData.add("Sverri er bedste Sylas i DK og FO");
+        FileData fileData = new FileData(testData,fileNameTest);
+        // fileData.saveFile();
+        ArrayList<String> testReadData = new ArrayList<>();
+        FileData readFileData = new FileData(testReadData, fileNameTest);
+        readFileData.readFile();
+        assertTrue(testData.equals(testReadData));
+        System.out.println(testData);
+
+    }
 
     @Test
     void readFile() {
