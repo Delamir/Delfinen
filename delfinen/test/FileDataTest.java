a97234a5c8e3c5c5dc0702a39f00f05a0a90d8de
Sverri <sverrireinert@gmail.com>
2020-12-03 11:34:10 +0100

    more warnings dealt with

--- delfinen/test/FileDataTest.java
+++ delfinen/test/FileDataTest.java
@@ -1,5 +1,7 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,17 +11,23 @@ class FileDataTest {
     /**
      * @author Sverri og Patrick
      */
+    @BeforeAll
+    static void before() {
+        File f = new File("test.ser");
+        f.delete();
+    }
+
     @Test
     void fileDataTest() {
         ArrayList<String> testData = new ArrayList<>();
         String fileNameTest = "test.ser";
         testData.add("Sverri er bedste Sylas i DK og FO");
-        FileData fileData = new FileData(testData,fileNameTest);
+        FileData<String> fileData = new FileData<>(testData,fileNameTest);
         fileData.saveFile();
         ArrayList<String> testReadData = new ArrayList<>();
-        FileData readFileData = new FileData(testReadData, fileNameTest);
+        FileData<String> readFileData = new FileData<>(testReadData, fileNameTest);
         testReadData = readFileData.readFile();
-        assertTrue(testData.equals(testReadData));
+        assertEquals(testReadData, testData);
         System.out.println(testReadData);
 
     }
