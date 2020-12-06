3357c314790f3960856854f314a754de2a9325b7
Kringelum2 <p.kringelum@gmail.com>
2020-12-06 11:21:32 +0100

    Test cashier

--- delfinen/test/delfinen/CashierTest.java
+++ delfinen/test/delfinen/CashierTest.java
@@ -27,12 +27,36 @@ class CashierTest {
 
     @Test
     void payArrearsTest() {
+        ScannerMethods.setIn("1\n");
+        Member m = new PassiveMember(22, "Bent");
+        Member m1 = new CompetitiveMember(50, "Karen", new ArrayList<>());
+        ArrayList<Member> members = new ArrayList<>();
+        members.add(m);
+        members.add(m1);
+        m.setPaymentStatus(false);
+        m1.setPaymentStatus(false);
 
+        Cashier cashier = new Cashier(members);
+        cashier.payArrears();
+        assertTrue(m.isPaymentStatus());
+        assertEquals(1, cashier.getArrearsMembers().size());
     }
 
     @Test
     void addMemberToArrearsTest() {
+        ScannerMethods.setIn("1\n");
+        Member m2 = new PassiveMember(30, "Bodil");
+        Member m = new PassiveMember(22, "Bent");
+        Member m1 = new CompetitiveMember(50, "Karen", new ArrayList<>());
+        ArrayList<Member> members = new ArrayList<>();
+        members.add(m);
+        members.add(m1);
+        members.add(m2);
 
+        Cashier cashier = new Cashier(members);
+        cashier.addMemberToArrears();
+        assertFalse(m.isPaymentStatus());
+        assertEquals(1, cashier.getArrearsMembers().size());
     }
 
 }
